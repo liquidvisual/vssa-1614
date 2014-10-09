9c814d0d5c24e990d31e73dd002a3d06cc6dd37f
Mitchell Warren <liquidvisual@gmail.com>
2014-10-09 11:51:54 +1100

    Built vacswimsa.com.au from commit 56c36d8 on branch master

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,17 +28,105 @@ if (isTouchDevice) {
         $('html, body').animate({ scrollTop: containerPos }, 300);
         }
     });
-
     // Trigger hamburger by touch on mobile - this eliminates glitch with FastClick.js
     $(".hamburger").css("visibility", "visible").bind("touchstart", function() { $("#off-canvas-menu").trigger("open.mm"); });
 
 } else {
-    // Start Foundation JS as normal with defaults
-    $(document).foundation();
-
     // Trigger hamburger with a click on desktop
     $(".hamburger").css("visibility", "visible").bind("click", function() { $("#off-canvas-menu").trigger("open.mm"); });
 }
+
+//-----------------------------------------------------------------
+// Google Map and Markers
+// http://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-multiple-marker-example
+// http://stackoverflow.com/questions/7084363/google-maps-in-hidden-div
+// http://jsfiddle.net/P2QhE/
+//-----------------------------------------------------------------
+
+var maps = [];
+
+// var addresses = ['27a Tallowwood Crescent, BRADBURY 2560', 'Sydney CBD'];
+
+var latLongs = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+var addresses = [
+    {name: 'mitches house',url:'http://google.com.au',address:'27a Tallowwood Crescent, BRADBURY 2560'},
+    {name: 'somewhere house',url:'http://bing.com.au',address:'42 Tallowwood Crescent, BRADBURY 2560'},
+];
+
+$(document).ready(function () {
+
+    $.each($('.google-map'), function(index, value){
+        var map;
+        var elevator;
+        var myOptions = {
+            zoom: 1,
+            center: new google.maps.LatLng(0, 0),
+            mapTypeId: 'terrain'
+        };
+        map = new google.maps.Map(value, myOptions);
+        maps.push(map);
+    });
+
+    // turn all our addresses into latlongs and push to maps;
+    $.each(addresses, function(index, value) {
+        console.log('calling google api for ' + value.address);
+        var url = 'http://maps.googleapis.com/maps/api/geocode/json';
+        $.ajax({
+          dataType: "json",
+          url: url,
+          data: 'address=' + value.address + '&sensor=false',
+          async: true
+        })
+        .done(function (data, textStatus, jqXHR) {
+            var p = data.results[0].geometry.location;
+            var newLatLong = [value.name, p.lat, p.lng, index];
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            latLongs.push(newLatLong);
+            console.log(newLatLong);
+            $.each($('.google-map'), function(index2, value2) {
+                new google.maps.Marker({
+                    position: latlng,
+                    map: value2
+                });
+            });
+        });
+    });
+});
+
+// Re-orient map
+function centerMap(map) {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
+}
+
+// Adjust Maps
+function adjustMaps(){
+    $.each(maps, function(index, value){
+        centerMap(value);
+    });
+}
+
+if ($('.google-map').length) {
+    // Call adjust on resize
+    $(window).resize(function() {
+        adjustMaps();
+    });
+}
+
+// Foundation tabs fires the adjusting of Maps
+$(document).foundation({
+    tab: {
+        callback : adjustMaps
+    }
+});
 
 //-----------------------------------------------------------------
 // 'MMenu' Settings
